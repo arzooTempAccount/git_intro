6aa81a1dc1e4a3d6a1e7bfcac2a9f402bebea0e1
arzooy97 <arzooy97@gmail.com>
2017-09-30 19:38:08 +0530

    Change to file01.js

--- file01.js
+++ file01.js
@@ -4,6 +4,7 @@ function greet(name)
     console.log('Hello ' + name)
     console.log('Hello ' + name)
     console.log('Hello ' + name)
+    console.log('I am a native of Gurugram');
 }
 
 greet('Arzoo')
